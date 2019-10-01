0b925f9743f561198618cc30e8f4596ec43ae749
vickris <christophervundi@gmail.com>
2019-10-01 15:45:04 +0300

    Suites not being returned

--- lib/poker.ex
+++ lib/poker.ex
@@ -27,14 +27,30 @@ defmodule Poker do
   def check_four_of_a_kind(poker_hand) do
   end
 
-  def check_straight_straight_flush(poker_hand) do
+  def check_straight_flush(poker_hand) do
+    poker_hand
+    |> String.split()
+    |> Enum.reduce(%{vals: [], suites: []}, fn x, acc ->
+      x |> String.codepoints() |> separate_values_from_suits(acc)
+    end)
+  end
+
+  def separate_values_from_suits([val, suit], acc) do
+    if !Enum.member?(acc.suites, suit), do: %{acc | suites: [suit | acc.suites]}
+    if !Enum.member?(acc.vals, val), do: %{acc | vals: [val | acc.vals]}
+    # if !Enum.member?(acc.suites, suit), do: %{acc | suites: [suit | acc.suites]}
   end
 
   def compare_hands(hand1, hand2) do
-    hand1_highest = getHighest(hand1)
-    hand2_highest = getHighest(hand2)
+    hand1_highest = get_highest(hand1)
+    hand2_highest = get_highest(hand2)
 
     if hand1_highest == hand2_highest do
     end
+  end
+
+  def get_highest(hand) do
+    hand
+    |> check_straight_flush
   end
 end
