044e21c173f4950c95f2bccc0373485c9a54561a
vickris <christophervundi@gmail.com>
2019-10-02 21:20:21 +0300

    Can determine type

--- lib/poker.ex
+++ lib/poker.ex
@@ -12,7 +12,12 @@ defmodule Poker do
   def check_two_pairs(poker_hand) do
   end
 
-  def check_three_of_a_kind(poker_hand) do
+  def check_three_of_a_kind(occurences) do
+    if Enum.member?(occurences, 1) do
+      {:ok, "two pairs"}
+    else
+      {:ok, "three of a kind"}
+    end
   end
 
   def check_straight(poker_hand) do
@@ -24,7 +29,12 @@ defmodule Poker do
   def check_full_house(poker_hand) do
   end
 
-  def check_four_of_a_kind(poker_hand) do
+  def check_four_of_a_kind(occurences) do
+    if Enum.member?(occurences, 4) do
+      {:ok, "four of a kind"}
+    else
+      {:ok, "full house"}
+    end
   end
 
   def get_suites_and_vals(poker_hand) do
@@ -41,15 +51,27 @@ defmodule Poker do
 
     case {suit_count, value_count} do
       {1, 5} -> {:ok, "straight flush"}
-      {_, 2} -> {:ok, "four of a kind"}
-      {_, 2} -> {:ok, "full house"}
+      {_, 2} -> confirm_type(2, values)
       {1, _} -> {:ok, "flush"}
       {_, 5} -> {:ok, "straight"}
-      {_, 3} -> {:ok, "three of a kind"}
-      {_, 3} -> {:ok, "two pairs"}
+      {_, 3} -> confirm_type(3, values)
       {_, 4} -> {:ok, "pair"}
       _ -> {:ok, "High Card"}
     end
+  end
+
+  def confirm_type(2, values) do
+    values
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    |> Map.values()
+    |> check_four_of_a_kind
+  end
+
+  def confirm_type(3, values) do
+    values
+    |> Enum.reduce(%{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    |> Map.values()
+    |> check_three_of_a_kind
   end
 
   def separate_values_from_suits([val, suit], acc) do
