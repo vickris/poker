2cc95d21dc1e406c9e66e1dee2a2a340371d5d58
vickris <christophervundi@gmail.com>
2019-10-05 16:48:19 +0300

    Test ties

--- test/poker_test.exs
+++ test/poker_test.exs
@@ -10,6 +10,10 @@ defmodule PokerTest do
     test "highest pair wins" do
       assert Poker.compare_hands("2H 3D 3S 9C KD", "2C 4H 4S 8C AH") == "White wins - 4"
     end
+
+    test "tie" do
+      assert Poker.compare_hands("2C 4H 4S 8C AH", "2C 4H 4S 8C AH") == "Tie"
+    end
   end
 
   test "highest pair wins" do
@@ -32,6 +36,10 @@ defmodule PokerTest do
     test "with same pairs ranked by remaining vals" do
       assert Poker.compare_hands("2H 2D 4S 4C KD", "2C 2H 4S 4C AH") == "White wins - A"
     end
+
+    test "tie" do
+      assert Poker.compare_hands("2H 2D 4S 4C KD", "2H 2D 4S 4C KD") == "Tie"
+    end
   end
 
   test "three of a kind" do
@@ -46,6 +54,10 @@ defmodule PokerTest do
     test "straight wins over pair" do
       assert Poker.compare_hands("2H 3D 3S 9C KD", "2C 3H 4S 8C AH") == "White wins - straight"
     end
+
+    test "tie" do
+      assert Poker.compare_hands("2C 3H 4S 8C AH", "2C 3H 4S 8C AH") == "Tie"
+    end
   end
 
   describe "flush" do
@@ -55,6 +67,10 @@ defmodule PokerTest do
 
     test "wins over pair" do
       assert Poker.compare_hands("2H 3D 3S 9C KD", "2H 4H 3H 8H AH") == "White wins - flush"
+    end
+
+    test "tie" do
+      assert Poker.compare_hands("2H 4H 8H 9H KH", "2H 4H 8H 9H KH") == "Tie"
     end
   end
 
@@ -66,6 +82,10 @@ defmodule PokerTest do
     test "wins over pair" do
       assert Poker.compare_hands("2H 2D 2S 2C KD", "2H 4H 3H 8H AH") ==
                "Black wins - four of a kind"
+    end
+
+    test "tie" do
+      assert Poker.compare_hands("2H 2D 2S 2C KD", "2H 2D 2S 2C KD") == "Tie"
     end
   end
 
